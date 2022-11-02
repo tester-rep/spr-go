588379754b72d62b9ce76a5edbb2d43ced11fc16
chuanwumao <chuanwumao@tencent.com>
2022-11-02 21:13:14 +0800

    fix bug

--- sampler/sampler.go
+++ sampler/sampler.go
@@ -1,6 +1,8 @@
 package sampler
 
+import "github.com/tester-rep/spr-go/abstract"
+
 type ISampler interface {
-	NewSamplerStart(key string) ISampler
+	NewSamplerStart(key string, robot abstract.IRobot) ISampler
 	SampleEnd(result bool)
 }
