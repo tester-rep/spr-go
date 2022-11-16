6a19792a8b143b2102296113eb5400e3ed6f0b6a
chuanwumao <chuanwumao@tencent.com>
2022-11-16 16:43:15 +0800

    增加sampler的api，可以手动设置起止时间

--- sampler/sampler.go
+++ sampler/sampler.go
@@ -1,8 +1,13 @@
 package sampler
 
-import "github.com/tester-rep/spr-go/abstract"
+import (
+	"github.com/tester-rep/spr-go/abstract"
+	"time"
+)
 
 type ISampler interface {
 	NewSamplerStart(key string, robot abstract.IRobot) ISampler
 	SampleEnd(result bool)
+	NewSamplerStartWithStartTime(key string, startTime time.Duration, robot abstract.IRobot) ISampler
+	SampleEndWithEndTime(result bool, endTime time.Duration)
 }
